70521efc5e97a360d2f4cca49437e1b0c70efbb5
lAntimat <tamitnal@gmail.com>
2018-04-27 22:33:57 +0300

    Initial commit

---
+++ app/src/main/java/ru/lantimat/testappvk/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.lantimat.testappvk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.util.VKUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        VKSdk.login(this, "what");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+            }
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
---
+++ app/src/main/java/ru/lantimat/testappvk/MyApplication.java
@@ -0,0 +1,14 @@
+package ru.lantimat.testappvk;
+
+import android.app.Application;
+
+import com.vk.sdk.VKSdk;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        VKSdk.initialize(getApplicationContext());
+    }
+}
