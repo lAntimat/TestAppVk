8edb3d19d67be088b9081edaec0d164f69966317
lAntimat <tamitnal@gmail.com>
2018-04-28 15:01:03 +0300

    first commit

--- app/src/main/java/ru/lantimat/testappvk/MainActivity.java
+++ app/src/main/java/ru/lantimat/testappvk/MainActivity.java
@@ -1,16 +1,41 @@
 package ru.lantimat.testappvk;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKObject;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.methods.VKApiUsers;
+import com.vk.sdk.api.model.VKApiApplicationContent;
+import com.vk.sdk.api.model.VKApiUser;
+import com.vk.sdk.api.model.VKApiUserFull;
+import com.vk.sdk.api.model.VKList;
+import com.vk.sdk.api.model.VKUsersArray;
 import com.vk.sdk.util.VKUtil;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ru.lantimat.testappvk.utils.DownloadImageTask;
+import ru.lantimat.testappvk.utils.ImagesCache;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvFirstName, tvLastName;
+    private ImageView ivProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +43,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
-        VKSdk.login(this, "what");
+
+        tvFirstName = findViewById(R.id.tvFirstName);
+        tvLastName = findViewById(R.id.tvLastName);
+        ivProfile = findViewById(R.id.imageView);
+
+        VKParameters params = VKParameters.from(VKApiConst.FIELDS, "photo_50, photo_200");
+        if(VKSdk.isLoggedIn()) {
+            VKRequest request = new VKRequest(VKApi.users().get().methodName, params) ;
+            request.executeWithListener(new VKRequest.VKRequestListener() {
+                @Override
+                public void onComplete(VKResponse response) {
+                    super.onComplete(response);
+                    //VKList<VKApiUser> users = (VKList<VKApiUser>) response.parsedModel;
+
+                    VKApiUser user;
+
+                    try {
+                        JSONArray jsonArray = response.json.getJSONArray("response");
+                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+                        user = new VKApiUser();
+                        user.parse(jsonObject);
+                        tvFirstName.setText(user.first_name);
+                        tvLastName.setText(user.last_name);
+                        ImagesCache cache = ImagesCache.getInstance();
+                        Bitmap bm = cache.getImageFromWarehouse(user.photo_200);
+
+                        if (bm != null) {
+                            ivProfile.setImageBitmap(bm);
+                        } else {
+                            ivProfile.setImageBitmap(null);
+
+                            DownloadImageTask imgTask = new DownloadImageTask(cache, ivProfile, 300, 300);//Since you are using it from `Activity` call second Constructor.
+
+                            imgTask.execute(user.photo_200);
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            } else {
+            VKSdk.login(this, "what");
+        }
+
+
     }
 
     @Override
--- app/src/main/java/ru/lantimat/testappvk/MyApplication.java
+++ app/src/main/java/ru/lantimat/testappvk/MyApplication.java
@@ -4,11 +4,15 @@ import android.app.Application;
 
 import com.vk.sdk.VKSdk;
 
+import ru.lantimat.testappvk.utils.ImagesCache;
+
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         VKSdk.initialize(getApplicationContext());
+
+        ImagesCache.getInstance().initializeCache();
     }
 }
